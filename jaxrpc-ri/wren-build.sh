8e4b0a0c4bb5a4179268bf83f09dc00013c889fe
Kortanul <kortanul@protonmail.com>
2018-08-27 02:44:06 -0400

    Avoid Running `maven-distribution` Twice

--- jaxrpc-ri/wren-build.sh
+++ jaxrpc-ri/wren-build.sh
@@ -6,4 +6,4 @@ export GPG_COMMAND='./wren-gpg.sh'
 export GPG_DEFAULT_KEY="D7F749B5"
 export GPG_KEY_PASSPHRASE="${D7F749B5_PASSPHRASE}"
 
-./build.sh clean maven-distribution $@
+./build.sh clean ${1:-maven-distribution}
