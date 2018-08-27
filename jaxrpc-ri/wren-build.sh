1e78940073a0b6d99a2819b6ea5ea483fa79f0db
Kortanul <kortanul@protonmail.com>
2018-08-27 02:33:46 -0400

    Add Maven Deployment (`maven-deploy`) Ant Target
    Allows new versions to be published to the Wren JFrog repo via `./wren-deploy.sh`. The `D7F749B5_PASSPHRASE` environment variable MUST be set beforehand (usually via the `wren-preload-creds` alias).

--- jaxrpc-ri/wren-build.sh
+++ jaxrpc-ri/wren-build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+set -e
+set -u
+
 export GPG_COMMAND='./wren-gpg.sh'
 export GPG_DEFAULT_KEY="D7F749B5"
 export GPG_KEY_PASSPHRASE="${D7F749B5_PASSPHRASE}"
 
-./build.sh clean maven-distribution
+./build.sh clean maven-distribution $@
